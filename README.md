683952af6ad882b18fdfbdb9e139f1aaf8f3edea
Gagan-Sky <gagankumar8294@gmail.com>
2024-07-16 22:24:20 +0530

    created class Component

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ These are all the things we going to learn |
 Implemented functions in this MovieListApp
 
 1. Basic React
+2. Creating Class Component
---
+++ src/MovieCard.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class MovieCard extends React.Component {
+    render() {
+        return (
+            <>
+                <h2>Movie Component</h2>
+            </>
+        )
+    }
+}
+
+export default MovieCard;
