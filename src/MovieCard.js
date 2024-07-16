b22f4f0a8eed27d4a8598b0810df8e8428643b44
Gagan-Sky <gagankumar8294@gmail.com>
2024-07-16 22:43:00 +0530

    constructor , created a state | constructor | handler function to add Stars | rating

--- src/MovieCard.js
+++ src/MovieCard.js
@@ -1,10 +1,27 @@
 import React from "react";
 
 class MovieCard extends React.Component {
+    
+    constructor() {
+        super();
+        this.state = {
+            title : 'Avengers',
+            plot: 'Supernatural powers',
+            stars : 0,
+        }
+    }
+
+    addStars = () => {
+        this.state.stars += 0.5;
+    }
+
+
     render() {
+        const { title , plot , stars } = this.state
         return (
             <>
                 <h2>Movie Component</h2>
+                <div></div>
             </>
         )
     }
