ac9106667e062acc74453f8a1ad5fcacb4f3a45f
Gagan-Sky <gagankumar8294@gmail.com>
2024-07-17 21:52:02 +0530

    created basic navabr compenent

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import MovieList from "./MovieList";
-
+import Navbar from "./Navbar";
 
 class App extends React.Component {
     render() {
         return (
             <>
+                <Navbar />
                 <h2>App Component</h2>
                <MovieList />
             </>
